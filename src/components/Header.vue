74a55d48968f1c464b6427ee2db28cf968ad4f2d
VAB1998 <blanco.vincenzo@outlook.com>
2021-09-24 15:42:30 +0200

    Header Component: Basic Structure Completed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,26 @@
 <template>
-    <header></header>
+    <header>
+        <div id="nav_bar">
+
+                <a class="logo" href="#">
+                    <img src="../assets/images/dc-logo.png" alt="DC logo">
+                </a>
+            
+
+                <ul class="menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Home</a></li>
+                    
+                </ul>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -8,4 +29,49 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../style/variables.scss";
+
+//Reset
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+#nav_bar{
+    // background-color: #ffffff;
+    font-family: $headerFont;
+    background-color: blue;
+    height: 110px;
+    //Flex Settings
+    display: flex;
+    justify-content: space-evenly;
+    
+    .logo{
+        align-self: center;
+
+        img{
+            width: 70px;
+        }
+    }
+
+    
+    .menu{
+        list-style-type: none;
+
+        //Flex Settings
+        display: flex;
+        li{
+            cursor: pointer;
+            
+            a{
+            text-decoration: none;
+            padding: 0 20px;
+            line-height: 110px;
+        
+            }
+        } 
+        
+    }
+}
 </style> 
