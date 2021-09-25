087c5946f7da0a5150d4253229fae5c52dafbbe4
VAB1998 <blanco.vincenzo@outlook.com>
2021-09-25 14:26:59 +0200

    Header component: Created the links objects

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,7 +23,66 @@
 </template>
 
 <script>
+export default{
+    name: 'Header',
+    data: function(){
+        return{
 
+            links: [
+                {
+                    text: 'characters',
+                    url: '#characters',
+                    current: false
+                },
+                {
+                    text: 'comics',
+                    url: '#comics',
+                    current: true
+                },
+                {
+                    text: 'movies',
+                    url: '#movies',
+                    current: false
+                },
+                {
+                    text: 'tv',
+                    url: '#tv',
+                    current: false
+                },
+                {
+                    text: 'games',
+                    url: '#games',
+                    current: false
+                },
+                {
+                    text: 'collectibles',
+                    url: '#collectibles',
+                    current: false
+                },
+                {
+                    text: 'videos',
+                    url: '#videos',
+                    current: false
+                },
+                {
+                    text: 'fans',
+                    url: '#fans',
+                    current: false
+                },
+                {
+                    text: 'news',
+                    url: '#news',
+                    current: false
+                },
+                {
+                    text: 'shop',
+                    url: '#shop',
+                    current: false
+                },
+            ]
+        }
+    }
+}
 </script>
 
 <style lang="scss" scoped>
@@ -79,8 +138,13 @@ header{
                         color: $primaryColor;
                     }
                 }
+                &.active{
+                    border-bottom: 4px solid $primaryColor;
+                    a{
+                        color: $primaryColor;
+                    }
+                }
             } 
-            
         }
     }
 }
