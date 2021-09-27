71baedad66d307280d342e201fb718f19344fa6e
VAB1998 <blanco.vincenzo@outlook.com>
2021-09-27 18:19:36 +0200

    Created a Navbar Component and rendered throught external datas

---
+++ src/components/Navbar.vue
@@ -0,0 +1,91 @@
+<template>
+    <div id="nav_bar">
+
+        <a class="logo" href="#">
+            <img src="../assets/images/dc-logo.png" alt="DC logo">
+        </a>
+
+        <ul class="menu">
+
+            <li :class="{active : link.current}" v-for="(link, index) in links" :key="index">
+                <a :href="link.url">{{link.text}}</a>
+            </li> 
+
+        </ul>
+    </div>
+</template>
+
+<script>
+import links from '../data/navbarLinks.js'
+export default {
+    name: 'Navbar',
+
+
+    data : function (){
+        return{
+            links
+            
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+@import "../style/general.scss";
+
+#nav_bar{
+    //Style
+    background-color: #ffffff;
+    //Spacing and Dimensions
+    height: 110px;
+    //Flex Settings
+    display: flex;
+    justify-content: space-between;
+    
+    .logo{
+        align-self: center;
+
+        img{
+            //Spacing and Sizing
+            width: 70px;
+        }
+    }
+
+    .menu{
+        //Style
+        list-style-type: none;
+        text-transform: uppercase;
+        //Flex Settings
+        display: flex;
+
+        li{
+            margin: 0 15px;
+            transition: .3s ease-in-out;
+
+            a{
+                //Style
+                text-decoration: none;
+                font-weight: 500;
+                color: $secondaryColor;
+                //Spacing and Sizing
+                line-height: 110px;
+                transition: .3s ease-in-out;                    
+            }
+
+            &:hover{
+                border-bottom: 4px solid $primaryColor;
+                a{
+                    color: $primaryColor;
+                }
+            }
+            &.active{
+                border-bottom: 4px solid $primaryColor;
+                a{
+                    color: $primaryColor;
+                }
+            }
+        } 
+    }
+}
+</style>
---
+++ src/data/navbarLinks.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        text: 'characters',
+        url: '#characters',
+        current: false
+    },
+    {
+        text: 'comics',
+        url: '#comics',
+        current: true
+    },
+    {
+        text: 'movies',
+        url: '#movies',
+        current: false
+    },
+    {
+        text: 'tv',
+        url: '#tv',
+        current: false
+    },
+    {
+        text: 'games',
+        url: '#games',
+        current: false
+    },
+    {
+        text: 'collectibles',
+        url: '#collectibles',
+        current: false
+    },
+    {
+        text: 'videos',
+        url: '#videos',
+        current: false
+    },
+    {
+        text: 'fans',
+        url: '#fans',
+        current: false
+    },
+    {
+        text: 'news',
+        url: '#news',
+        current: false
+    },
+    {
+        text: 'shop',
+        url: '#shop',
+        current: false
+    },
+]
