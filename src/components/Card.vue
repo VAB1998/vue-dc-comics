b0303ec23e7a28db7d77e57eb56cbfba1c5a41da
VAB1998 <blanco.vincenzo@outlook.com>
2021-09-27 16:24:25 +0200

    Added Props

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,23 +1,32 @@
 <template>
-    <div class="card_element"></div>
+    <div class="card_element">
+        <img :src="imageSource" alt="series">
+        <h4>{{series}}</h4>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    name : 'Card',
+    props : ['series', 'imageSource']
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .card_element{
-    /* Style */
-    background-color: indianred;
-    border: 1px solid black;
-    /* Spacing and Dimensions */
-    height: 100px;
+    //Style
+    color: #ffff;
+    //Spacing and Dimensions 
     margin: 5px;
 
-    /* Flex Settings */
+    //Flex Settings 
     flex-basis: calc(100% / 6 - 10px);
+
+    img {
+        width: 100%;
+        height: 161px;
+        object-fit: cover;
+        object-position: top;
+    }
 }
 </style>
